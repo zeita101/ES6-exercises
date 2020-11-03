e875d0cda2fce57e05fbc8f997c3a5522be38bde
zeita101 <zeitajb1@gmail.com>
2020-11-02 21:25:45 -0500

    Completed

--- main.js
+++ main.js
@@ -4,6 +4,11 @@
 // Using the spread operator, make a new
 // array with numbers that contains all of the evens and odds
 
+let arrEven = [0, 2, 4, 6, 8];
+let arrOdd = [1, 3, 5, 7, 9];
+
+const newArr = [...arrOdd, ...arrEven] ;
+console.log(...newArr);
 
 
 //Exercise #2
@@ -15,7 +20,11 @@
 // Use the spread operator to build a final allFoods with the 
 // items from the two objects you just built
 
+favFood = { sushi: 'Rakuzen', fries: 'McDonalds', cheesecake: 'Costco' }
+friendFavFood = { pizza: '2 Bros', cake: 'Lords Bakery', potatoes: 'Loaded Potato' }
 
+food = {...favFood, ...friendFavFood };
+console.log(food);
 
 // Exercise #3
 // Define your favorite movie character in an object
@@ -30,3 +39,24 @@
 // 2) define three variables (age, breed, name) outside of the object
 // 3) add those variables to your pet
 // log the most important thing about it: the pet's name 
+
+
+favMovie = {
+  name: 'Moses', 
+  age: '24', 
+  hometown: 'Egypt'
+}
+const { name, age, ...me } = favMovie
+console.log(name, age, me.hometown);
+
+const pet = {}
+favMovie = { ...favMovie, pet }
+let petName = 'Mickey'
+let petAge = 7
+let petBreed = 'Huskie'
+favMovie.pet.Name = petName
+favMovie.pet.Age = petAge
+favMovie.pet.Breed = petBreed
+console.log(favMovie.pet.Name)
+
+
